a11d9db4b29e48c43e0cca297d10a80b15c2ec56
Antonio <antoniodeveloper22@gmail.com>
2022-07-30 17:10:25 -0300

    Projeto finalizado

--- Motor5/src/main.cpp
+++ Motor5/src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
 
+#define motorVel 5
+#define motorIn1 6
+#define motorIn2 7
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode( motorVel, OUTPUT );
+  pinMode( motorIn1, OUTPUT );
+  pinMode( motorIn2, OUTPUT );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite( motorIn1, HIGH );
+  digitalWrite( motorIn2, LOW );
+
+  for( int vel = 0; vel <= 50; vel++ ){
+    analogWrite( motorVel, vel );
+    delay( 10 );
+  }
+
+  delay( 2000 ); //Motor gira no sentido horário até a vélocidade máxima por 2s
+
+  digitalWrite( motorIn1, LOW );
+  digitalWrite( motorIn2, HIGH );
+
+  for( int vel = 50; vel >= 0; vel-- ){
+    analogWrite( motorVel, vel );
+    delay( 100 );
+  }
+
+  delay( 2000 ); //Motor gira no sentido anti-horário até a vélocidade mínnima por 2s
 }
