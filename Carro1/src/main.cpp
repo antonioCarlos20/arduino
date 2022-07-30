00e948c88845965baeea7687320e8e9c9ae35a36
Antonio <antoniodeveloper22@gmail.com>
2022-07-30 17:58:49 -0300

    Carrinho anda infinitamente em um único sentido

---
+++ Carro1/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+#define motor1Vel 5
+#define motor1In1 6
+#define motor1In2 7
+#define motor2Vel 9
+#define motor2In1 10
+#define motor2In2 11
+
+void setup() {
+  pinMode( motor1Vel, OUTPUT );
+  pinMode( motor1In1, OUTPUT );
+  pinMode( motor1In2, OUTPUT );
+  pinMode( motor2Vel, OUTPUT );
+  pinMode( motor2In1, OUTPUT );
+  pinMode( motor2In2, OUTPUT );
+}
+
+void loop() {
+  digitalWrite( motor1In1, HIGH );
+  digitalWrite( motor1In2, LOW );
+  digitalWrite( motor2In1, HIGH );
+  digitalWrite( motor2In2, LOW );
+
+  analogWrite( motor1Vel, 255 );
+  analogWrite( motor2Vel, 255 );
+}
