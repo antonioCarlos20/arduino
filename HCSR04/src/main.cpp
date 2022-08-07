fadbeed1db00dccdda774f7d6feaf4fda193b7db
Antônio <afernandesdecampos50@gmail.com>
2022-08-07 13:44:48 -0300

    Primeiro teste com o sensor HCSR04 realizado com sucesso!

--- HCSR04/src/main.cpp
+++ HCSR04/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
 
+#define trig 3
+#define echo 2
+
+long duration;
+int distance;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode( trig, OUTPUT );
+  pinMode( echo, INPUT );
+
+  Serial.begin( 9600 );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite( trig, HIGH);
+
+  duration = ( pulseIn( echo, HIGH ) / 2);
+  distance = duration * 0.034;
+
+  Serial.println( "Distance:" );
+  Serial.print( distance );
+  Serial.println( "cm" );
+
+  delay( 1000 );
 }
