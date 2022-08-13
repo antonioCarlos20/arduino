abb6a22d630e67a7ca5a415a4ee7a922cf8b5813
Antônio <antoniodeveloper22@gmail.com>
2022-08-13 13:32:42 -0300

    Definindo as variáveis

--- Servo_HCSR04/src/main.cpp
+++ Servo_HCSR04/src/main.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+#define trig 3
+#define echo 2
+
+long duration;
+int dist, distance;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode( trig, OUTPUT);
+  pinMode( echo, INPUT);
 }
 
 void loop() {
