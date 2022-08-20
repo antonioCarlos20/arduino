8236cb341f7d871e22070f9273c8901815f4ed16
Antônio <antoniodeveloper22@gmail.com>
2022-08-20 07:42:05 -0300

    Tentando resolver o problema de comunicação dos motores

--- Carro2/src/main.cpp
+++ Carro2/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <Servo.h>
 
 #define motor1Vel 5
 #define motor1In1 6
@@ -7,14 +6,6 @@
 #define motor2Vel 9
 #define motor2In1 10
 #define motor2In2 11
-
-#define trig 3
-#define echo 2
-
-long duration;
-int dist, distance, distEsq, distDir, distFrente;
-
-Servo myservo;
 
 void frente( int vel){
   digitalWrite( motor1In1, HIGH );
@@ -66,28 +57,6 @@ void freia( int vel){
   analogWrite( motor2Vel, vel );
 }
 
-int mesureDistance(){
-  digitalWrite( trig, LOW );
-  delayMicroseconds( 10 );
-  digitalWrite( trig, HIGH );
-  delayMicroseconds( 10 );
-  digitalWrite( trig, LOW );
-
-  duration = ( pulseIn( echo, HIGH ) / 2 );
-  dist = duration * 0.034;
-
-  return dist;
-}
-
-void find(){
-  myservo.write( 135 );
-  delayMicroseconds( 20 );
-
-  delay( 1000 );
-  myservo.write( 45 );
-  delay( 1000 );
-  myservo.write( 90 );
-}
 
 void setup() {
   pinMode( motor1Vel, OUTPUT );
@@ -96,15 +65,23 @@ void setup() {
   pinMode( motor2Vel, OUTPUT );
   pinMode( motor2In1, OUTPUT );
   pinMode( motor2In2, OUTPUT );
-
-  pinMode( trig, OUTPUT);
-  pinMode( echo, INPUT);
-  myservo.attach( 9 );
-  myservo.write( 90 );
-
-  freia( 0 );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  frente( 125 );
+  delay( 2000 );
+  freia( 0 );
+  delay( 1000 );
+  re( 125 );
+  delay( 2000 );
+  freia( 0 );
+  delay( 1000 );
+  esquerda( 125 );
+  delay( 2000 );
+  freia( 0 );
+  delay( 1000 );
+  direita( 125 );
+  delay( 2000 );
+  freia( 0 );
+  delay( 1000 );
 }
