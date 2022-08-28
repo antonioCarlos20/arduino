d567484131f74cf151d84f97fc82619f7da62137
antonio <antonio@debian-BULLSEYE-live-builder-AMD64>
2022-08-28 18:01:45 -0300

    alteração nas portas dos motores e rotação do servo

--- Carro2/src/main.cpp
+++ Carro2/src/main.cpp
@@ -1,17 +1,21 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-#define motor1Vel 5
-#define motor1In1 6
-#define motor1In2 7
-#define motor2Vel 9
-#define motor2In1 10
-#define motor2In2 11
+#define motor1Vel 3
+#define motor1In1 2
+#define motor1In2 1
+#define motor2Vel 11
+#define motor2In3 13
+#define motor2In4 12
+
+#define pinServo 10
+Servo myservo;
 
 void frente( int vel){
   digitalWrite( motor1In1, HIGH );
   digitalWrite( motor1In2, LOW );
-  digitalWrite( motor2In1, HIGH );
-  digitalWrite( motor2In2, LOW );
+  digitalWrite( motor2In3, HIGH );
+  digitalWrite( motor2In4, LOW );
 
   analogWrite( motor1Vel, vel );
   analogWrite( motor2Vel, vel );
@@ -20,8 +24,8 @@ void frente( int vel){
 void re( int vel){
   digitalWrite( motor1In1, LOW );
   digitalWrite( motor1In2, HIGH );
-  digitalWrite( motor2In1, LOW );
-  digitalWrite( motor2In2, HIGH );
+  digitalWrite( motor2In3, LOW );
+  digitalWrite( motor2In4, HIGH );
 
   analogWrite( motor1Vel, vel );
   analogWrite( motor2Vel, vel );
@@ -30,8 +34,8 @@ void re( int vel){
 void esquerda( int vel){
   digitalWrite( motor1In1, LOW );
   digitalWrite( motor1In2, HIGH );
-  digitalWrite( motor2In1, HIGH );
-  digitalWrite( motor2In2, LOW );
+  digitalWrite( motor2In3, HIGH );
+  digitalWrite( motor2In4, LOW );
 
   analogWrite( motor1Vel, vel );
   analogWrite( motor2Vel, vel );
@@ -40,21 +44,31 @@ void esquerda( int vel){
 void direita( int vel){
   digitalWrite( motor1In1, HIGH );
   digitalWrite( motor1In2, LOW );
-  digitalWrite( motor2In1, LOW );
-  digitalWrite( motor2In2, HIGH );
+  digitalWrite( motor2In3, LOW );
+  digitalWrite( motor2In4, HIGH );
 
   analogWrite( motor1Vel, vel );
   analogWrite( motor2Vel, vel );
 }
 
-void freia( int vel){
-  digitalWrite( motor1In1, LOW );
-  digitalWrite( motor1In2, LOW );
-  digitalWrite( motor2In1, LOW );
-  digitalWrite( motor2In2, LOW );
+void freia(){
+  digitalWrite( motor1In1, HIGH );
+  digitalWrite( motor1In2, HIGH );
+  digitalWrite( motor2In3, HIGH );
+  digitalWrite( motor2In4, HIGH );
 
-  analogWrite( motor1Vel, vel );
-  analogWrite( motor2Vel, vel );
+  analogWrite( motor1Vel, 255 );
+  analogWrite( motor2Vel, 255 );
+}
+
+void rotate(){
+  myservo.write( 23 );
+  delay( 500 );
+  myservo.write( 158 );
+  delay( 500 );
+  myservo.write( 90 );
+  delay( 500 );
+
 }
 
 
@@ -63,25 +77,32 @@ void setup() {
   pinMode( motor1In1, OUTPUT );
   pinMode( motor1In2, OUTPUT );
   pinMode( motor2Vel, OUTPUT );
-  pinMode( motor2In1, OUTPUT );
-  pinMode( motor2In2, OUTPUT );
+  pinMode( motor2In3, OUTPUT );
+  pinMode( motor2In4, OUTPUT );
+
+  myservo.attach( pinServo );
+  myservo.write( 90 );
+
+  freia();
+
+  rotate();
 }
 
 void loop() {
   frente( 125 );
   delay( 2000 );
-  freia( 0 );
+  freia();
   delay( 1000 );
   re( 125 );
   delay( 2000 );
-  freia( 0 );
+  freia();
   delay( 1000 );
   esquerda( 125 );
   delay( 2000 );
-  freia( 0 );
+  freia();
   delay( 1000 );
   direita( 125 );
   delay( 2000 );
-  freia( 0 );
+  freia();
   delay( 1000 );
 }
